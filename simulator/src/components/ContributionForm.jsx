369b06f62b9b29752dbbb5b6c1978af42211f719
William Pei <william.pei@syndeticom.com.au>
2019-06-21 21:12:42 +1000

    Got form working

--- simulator/src/components/ContributionForm.jsx
+++ simulator/src/components/ContributionForm.jsx
@@ -1,71 +1,108 @@
 import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDollarSign } from "@fortawesome/free-solid-svg-icons";
+import InputRange from "react-input-range";
 
 class ContributionForm extends Component {
   constructor(props) {
     super(props);
-    this.state = { fundTotal: "", contributionAmount: "" };
+    this.state = {
+      fundTotal: "",
+      contributionPercentage: "",
+      fundBoundaries: {
+        minAmount: 500000,
+        maxAmount: 1000000
+      },
+      contributionBoundaries: {
+        minAmount: 1.1,
+        maxAmount: 7.6
+      }
+    };
   }
 
   componentDidMount() {
-    const MIN_FUND_AMOUNT = 500000;
-    const MAX_FUND_AMOUNT = 1000000;
-    const MIN_CONTRIBUTION_AMOUNT = 4;
-    const MAX_CONTRIBUTION_AMOUNT = 12;
+    const MIN_FUND_AMOUNT = this.state.fundBoundaries.minAmount;
+    const MAX_FUND_AMOUNT = this.state.fundBoundaries.maxAmount;
+    const MIN_CONTRIBUTION_AMOUNT = this.state.contributionBoundaries.minAmount;
+    const MAX_CONTRIBUTION_AMOUNT = this.state.contributionBoundaries.maxAmount;
 
     function getRandomFundTotal(max) {
       return (Math.random() * max + MIN_FUND_AMOUNT).toFixed(2);
     }
 
-    function getRandomContributionAmount(max) {
-      return (Math.random() * max + MIN_CONTRIBUTION_AMOUNT).toFixed(1);
+    function getRandomcontributionPercentage(max) {
+      return Math.random() * max + MIN_CONTRIBUTION_AMOUNT;
     }
+
+    var total = getRandomFundTotal(MAX_FUND_AMOUNT);
+    var contribution = getRandomcontributionPercentage(MAX_CONTRIBUTION_AMOUNT);
+    this.setState({ fundTotal: total, contributionPercentage: contribution });
+  }
+
+  render() {
+    var fundTotal = this.state.fundTotal;
+    var fundTotalAsFloat = parseFloat(fundTotal);
+    var contributionPercentage = parseFloat(this.state.contributionPercentage);
+    var contributionPercentageAsDecimal = contributionPercentage / 100;
+    var contributionAmount = (fundTotalAsFloat * contributionPercentageAsDecimal).toFixed(2);
 
     function numberWithCommas(number) {
       return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
 
-    var total = numberWithCommas(getRandomFundTotal(MAX_FUND_AMOUNT));
-    var contribution = getRandomContributionAmount(MAX_CONTRIBUTION_AMOUNT);
-    this.setState({ fundTotal: total, contributionAmount: contribution });
-  }
-
-  render() {
-    var fundTotal = this.state.fundTotal;
-    var contributionAmount = this.state.contributionAmount;
+    function updateFunction(value) {
+      console.log(fundTotal);
+      console.log(value);
+    }
 
     return (
-      <div className="contribution-form">
-        <div className="form-group">
-          <label htmlFor="fund-total" className="input-label">
-            Fund Total
-          </label>
-          <div className="input-group">
-            <span>
+      <section className="simulator-form">
+        <div className="contribution-form">
+          <div className="form-group">
+            <h1>
+              <label htmlFor="fund-total-display" className="input-label">
+                Your Balance
+              </label>
+            </h1>
+            <div className="input-group" id="fund-total">
               <FontAwesomeIcon icon={faDollarSign} />
-            </span>
-            <input className="form-input" id="fund-total" defaultValue={fundTotal} />
+              <input
+                className="form-input"
+                id="fund-total-display"
+                defaultValue={numberWithCommas(fundTotal)}
+                onChange={x => this.setState({ fundTotal: x.target.value })}
+              />
+            </div>
+          </div>
+          <div className="form-group">
+            <h1>
+              <label id="contribution-percentage-label" className="input-label">
+                Contribution Percentage
+              </label>
+            </h1>
+            <InputRange
+              id="contribution-percentage-input"
+              aria-labelledby="contribution-percentage-label"
+              formatLabel={value => value.toFixed(1) + "%"}
+              maxValue={10}
+              minValue={0.1}
+              value={contributionPercentage}
+              step={0.1}
+              onChange={value => this.setState({ contributionPercentage: value })}
+            />
           </div>
         </div>
-        <div className="form-group">
-          <label htmlFor="contribution-percentage" className="input-label">
-            Contribution Percentage
-          </label>
-          <input
-            className="form-input"
-            type="range"
-            id="contribution-percentage"
-            min="0"
-            max="20"
-            step="0.1"
-            defaultValue={contributionAmount}
-          />
-          <output htmlFor="contribution-percentage" id="contribution-percentage-output">
-            {contributionAmount}
-          </output>
+        <div className="contribution-results">
+          <div className="form-group">
+            <h1>Your Contribution</h1>
+            <span>change your contribution percentage to adjust</span>
+            <h2>
+              <FontAwesomeIcon icon={faDollarSign} />
+              <span id="contribution-result">{numberWithCommas(contributionAmount)}</span>
+            </h2>
+          </div>
         </div>
-      </div>
+      </section>
     );
   }
 }
--- simulator/src/scripts/contributionForm.js
+++ simulator/src/scripts/contributionForm.js
@@ -2,15 +2,7 @@ import $ from "jquery";
 
 $(function() {
   $(document).ready(function() {
-    $("#contribution-percentage").on("input", function(e) {
-      const $output = $("#contribution-percentage-output");
-      const $input = $(this);
-      var inputVal = $input.val();
-      var formattedVal = parseFloat(inputVal).toFixed(1);
-      $output.val(formattedVal);
-    });
-
-    $("#fund-total").on("blur", function(e) {
+    $("#fund-total-display").on("blur", function(e) {
       const $input = $(this);
       var inputVal = $input.val();
       var formattedVal = inputVal.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
